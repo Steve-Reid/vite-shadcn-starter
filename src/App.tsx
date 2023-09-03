9e9a42119040c4e9b5e7900be6b09079b3f3e2cf
SteveReid <steve@appby.design>
2023-09-03 19:24:40 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <Button size={'lg'} variant={'outline'}>
+        Click me!
+      </Button>
+    </div>
+  );
+}
+
+export default App;
